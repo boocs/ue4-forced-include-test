0af57a2ebe86602de07de5e2e394f73ad000e1de
boocs <62588629+boocs@users.noreply.github.com>
2021-01-26 14:03:27 -0700

    UE4 forced include test

---
+++ ue4-forced-include-test/Main/Intermediate/Build/Win64/MainGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h
@@ -0,0 +1,4 @@
+
+// In UE 4.26 EngineSharedPCH.h has 600+ #include paths
+
+#include "../../Secondary/Engine/Source/Runtime/Engine/Public/EngineSharedPCH.h"
---
+++ ue4-forced-include-test/Secondary/Engine/Source/Runtime/Engine/Public/EngineSharedPCH.h
@@ -0,0 +1,5 @@
+#pragma once
+
+// In UE 4.26 EngineSharedPCH.h has 600+ #include paths
+
+#include "GenericPlatform/ICursor.h"
