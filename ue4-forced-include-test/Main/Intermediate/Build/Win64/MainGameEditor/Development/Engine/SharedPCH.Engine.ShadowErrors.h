391b1653820f8042beedcb98914746947495426a
boocs <62588629+boocs@users.noreply.github.com>
2021-01-27 22:08:29 -0700

    Fixed forced include path in code-workspace Added some comments

--- ue4-forced-include-test/Main/Intermediate/Build/Win64/MainGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h
+++ ue4-forced-include-test/Main/Intermediate/Build/Win64/MainGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h
@@ -1,4 +1,6 @@
 
-// In UE 4.26 EngineSharedPCH.h has 600+ #include paths
+// In UE 4.26, EngineSharedPCH.h has 600+ #include paths
 
+
+// This is an absolute path in UE4
 #include "../../Secondary/Engine/Source/Runtime/Engine/Public/EngineSharedPCH.h"
